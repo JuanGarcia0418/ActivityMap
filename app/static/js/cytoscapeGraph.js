6a5cfe6e4b30843925b0c6af31617abedd45d372
Luiyi_FHB <luiyifernando5@gmail.com>
2023-02-16 10:29:52 -0500

    add all requirements files for the graph

---
+++ app/static/js/cytoscapeGraph.js
@@ -0,0 +1,107 @@
+function Graph() {
+  const containerRef = React.useRef(null);
+  const [elements, setElements] = React.useState(null);
+  const [selectedNode, setSelectedNode] = React.useState(null);
+
+  // // extract information from API
+  React.useEffect(() => {
+    fetch('http://localhost:8000/graphdata')
+      .then(response => response.json())
+      .then(elements => setElements(elements));
+  }, []);
+
+  // // create cystoscape container
+  React.useEffect(() => {
+    const cy = cytoscape({
+      container: containerRef.current,
+      elements: elements,
+      style: [{
+        selector: 'node',
+        style: {
+          'background-color': '#73c7f5',
+          'width': 150,
+          'height': 150,
+          'border-color': '#000',
+          'border-width': 5,
+          'border-opacity': .9,
+          // // Text into node
+          'label': 'data(name)',
+          'font-size': 26,
+          'text-halign': 'center',
+          'text-valign': 'center',
+          'color': '#000'
+        }
+      },
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'line-color': 'black',
+          'curve-style': 'bezier',
+          // 'target-arrow-color': 'black',
+          // 'target-arrow-shape': 'triangle',
+        }
+      }],
+      layout: {
+        name: 'breadthfirst',
+        fit: true, directed: true, padding: 30, grid: true, spacingFactor: 1.70,
+        roots: undefined, depthSort: undefined, animate: true, animationDuration: 500,
+        // // Get a central position for the graph
+        transform: function (node, position) { return position; }
+      }
+    });
+
+    // // Set listenesr for diferents action
+    cy.on("mouseover", "node", function (event) {
+      const node = event.target;
+      node.style({
+        'background-color': '#5a9fc4'
+      });
+    });
+    cy.on("mouseout", "node", function (event) {
+      const node = event.target;
+      node.style({
+        'background-color': '#73c7f5'
+      });
+    });
+    cy.on("tap", "node", function (event) {
+      const node = event.target;
+      setSelectedNode({
+        id: node.id(),
+        name: node.data("name"),
+        date: node.data("date"),
+        description: node.data("description")
+      });
+    });
+  }, [elements]);
+
+  return (
+    React.createElement('div', { className: "App" },
+      React.createElement('div', { ref: containerRef, className: "cytoscape-container" }),
+      selectedNode && React.createElement(Popup, { node: selectedNode, onClose: () => setSelectedNode(null) })
+    ));
+};
+
+function Popup({ node, onClose }) {
+  return (
+    React.createElement("div", { className: "popup" },
+      React.createElement("div", { className: "popup-content" },
+        React.createElement("h2", null, "Test Information"),
+        React.createElement("div", { className: "node-info" },
+          React.createElement("p", null,
+            React.createElement("span", null, node.name),
+            React.createElement("br", null),
+            node.date
+          ),
+          React.createElement("p", null,
+            React.createElement("span", null, "Test result"),
+            React.createElement("br", null),
+            node.description
+          )
+        ),
+        React.createElement("button", { onClick: onClose }, "Close")
+      )
+    ));
+};
+
+ReactDOM.render(React.createElement(Graph), document.getElementById("activity-map"));
