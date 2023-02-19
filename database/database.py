61e6132a4a005c6085e4ad3bda90d7bf1dbba742
Luiyi_FHB <luiyifernando5@gmail.com>
2023-02-18 23:40:26 -0500

    add PIP82

--- database/database.py
+++ database/database.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = 'mysql+mysqlconnector://root:root@localhost/DB7WS'
 engine = create_engine(DATABASE_URL)
-Session=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- database/models.py
+++ database/models.py
@@ -5,18 +5,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(Base):
-    __tablename__='user'
-    id = Column(Integer,primary_key=True, nullable=False)
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, nullable=False)
     username = Column(String(20), nullable=False)
     password = Column(CHAR(102), nullable=False)
     fullname = Column(String(50), nullable=False)
     user_type = Column(String(10), nullable=False)
     projects = relationship('projects', backref='author', lazy='dynamic')
-    activies = relationship('activities', backref='activity', lazy='dynamic')
+    activities = relationship('activities', backref='activity', lazy='dynamic')
 
 
 class Projects(Base):
-    __tablename__= 'projects'
+    __tablename__ = 'projects'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(20), nullable=False)
     date = Column(Date, nullable=False)
@@ -26,10 +26,8 @@ class Projects(Base):
 
 
 class Activity(Base):
-    __tablename__= 'activities'
+    __tablename__ = 'activities'
     name = Column(String(20), primary_key=True, nullable=False)
     date = Column(Date, nullable=False)
     description = Column(Text, nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-
-
