b036e9e0801ae8375dd809d6c8031e121d7f69fc
Luiyi_FHB <luiyifernando5@gmail.com>
2023-02-20 19:24:38 -0500

    add projectuser for see all projects to the user and change name for test by create_user

--- database/create_user.py
+++ database/create_user.py
@@ -8,7 +8,10 @@ mydb = mysql.connector.connect(
     passwd="root",
     database="DB7WS"
 )
-
+username = input("add the user name: ")
+password = input("add password: ")
+fullname = input("add the fullname: ")
+user_type = input("add rol for the user: ")
 
 # Crear cursor
 cursor = mydb.cursor()
@@ -18,12 +21,9 @@ id = uuid.uuid4()
 
 # Insertar datos con UUID
 consulta = "INSERT INTO user(id, username, password, fullname, user_type) VALUES (%s,%s,%s,%s,%s)"
-values = (str(id),'Test', 'test', 'User test', 'test')
+values = (str(id), username, password, fullname, user_type)
 cursor.execute(consulta, values)
 
 # Hacer commit y cerrar conexión
 mydb.commit()
 mydb.close()
-
-
-
