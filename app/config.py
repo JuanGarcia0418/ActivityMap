65610a22b4742ef32f9859ab5953db1c702193d1
Luiyi Hurtado <75503930+Luiyi-F@users.noreply.github.com>
2023-02-27 13:43:53 -0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class DevelopmentConfig(Config):
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'root'
-    MYSQL_DB = 'DB7WS'
+    MYSQL_DB = 'activityMap'
 
 
 config = {
